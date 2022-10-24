815b9d24343975c78ea1738bfe3eed2f28369815
MatV5 <mattikryszek@gmail.com>
2022-10-24 13:11:21 +0200

    Pierwszy commit, po dodaniu konstruktorów domyslnego i parametrycznego oraz setterow i getterow

--- main.cpp
+++ main.cpp
@@ -1,22 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
 class Wektor2D
 {
     public:
         Wektor2D ()
         {
             wektor[0] = wektor[1] = 0;
+            cout<<"domyslny"<<endl;
         }
         Wektor2D (float x, float y)
         {
             wektor[0] = x;
             wektor[1] = y;
+            cout<<"parametryczny"<<endl;
         }
         void setX(float var)
         {
             wektor[0]=  var;
+            cout<<"setterx"<<endl;
         }
         void setY(float var)
         {
             wektor[0]=  var;
+            cout<<"settery"<<endl;
         }
     private:
         float wektor[2];
