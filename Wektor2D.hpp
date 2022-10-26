519cf5341044d085382766a3582e6cd943b6cfc1
MatV5 <mattikryszek@gmail.com>
2022-10-26 22:35:07 +0200

    proba dodania operatorow, zmiana z wektora na dwie oddzielne zmienne

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -2,33 +2,37 @@ class Wektor2D
 {
     public:
     //Konstruktory
-        Wektor2D ()
-        {
-            wektor[0] = wektor[1] = 0;
-        }
-        Wektor2D (double x, double y)
-        {
-            wektor[0] = x;
-            wektor[1] = y;
-        }
+        Wektor2D() : x1(0), x2(0) {}
+        Wektor2D(double x, double y) : x1(x), x2(y) {}
+        Wektor2D(const Wektor2D& f) : x1(f.x1), x2(f.x2) {}
     //Settery
         void setX(double var)
         {
-            wektor[0]=  var;
+            x1=  var;
         }
         void setY(double var)
         {
-            wektor[0]=  var;
+            x2=  var;
         }
     //Gettery    
         double getX()
         {
-            return wektor[0];
+            return x1;
         }
         double getY()
         {
-            return wektor[1];
+            return x2;
+        }
+    //Operatory
+        Wektor2D operator+ (const Wektor2D& prawy)
+        {
+            Wektor2D tmp;
+            tmp.x1 = prawy.x1;
+            tmp.x1 =+ this->x1;
+            tmp.x2 = prawy.x2;
+            tmp.x2 =+ this->x2;
         }
     private:
-        double wektor[2];
+        double x2;
+        double x1;
 };
