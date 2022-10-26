3d8ef25e4a0d88a0e55dbe3f3dcf4a2b95b404ce
MatV5 <mattikryszek@gmail.com>
2022-10-26 23:20:30 +0200

    dodano iloczyn skalarny

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -28,9 +28,20 @@ class Wektor2D
         {
             Wektor2D tmp;
             tmp.x1 = prawy.x1;
-            tmp.x1 =+ this->x1;
+            tmp.x1 += this->x1;
             tmp.x2 = prawy.x2;
-            tmp.x2 =+ this->x2;
+            tmp.x2 += this->x2;
+            return tmp;
+        }
+
+        double operator* (const Wektor2D& prawy)
+        {
+            double tmp1, tmp2;
+            tmp1 = prawy.x1;
+            tmp1 *= this->x1;
+            tmp2= prawy.x2;
+            tmp2 *= this->x2;
+            return tmp1+tmp2;
         }
     private:
         double x2;
